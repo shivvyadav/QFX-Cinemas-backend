12dc8d4e61026bac3766e0f9c37f602a322a4b6d
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-15 16:35:03 +0545

    add server

---
+++ server.js
@@ -0,0 +1,39 @@
+import "dotenv/config";
+
+import express from "express";
+import {clerkMiddleware} from "@clerk/express";
+import cors from "cors";
+import mongoose from "mongoose";
+import axios from "axios";
+import {serve} from "inngest/express";
+import {inngest, functions} from "./inngest/index.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const URL = process.env.MONGO_URL;
+
+// Middleware
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(
+  cors({
+    origin: ["http://localhost:8000", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({client: inngest, functions}));
+
+// Connect to MongoDB
+mongoose
+  .connect(URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
