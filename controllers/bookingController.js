8981dc52f7dd0245689e614393fee9d516b467d5
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-19 23:41:20 +0545

    add booking controller for saving and getting the booking data of user

---
+++ controllers/bookingController.js
@@ -0,0 +1,92 @@
+import booking from "../models/booking.js";
+import Show from "../models/Show.js";
+
+export const createBooking = async (req, res) => {
+  try {
+    const userId = req.auth().userId;
+    const {show, seats, totalAmount, paymentMethod, date, time} = req.body;
+
+    if (!userId) return res.status(401).json({success: false, message: "Unauthorized"});
+
+    const foundShow = await Show.findById(show);
+    if (!foundShow) return res.status(404).json({success: false, message: "Show not found"});
+
+    // Create booking
+    const newBooking = new booking({
+      user: userId,
+      show,
+      seats,
+      totalAmount,
+      paymentMethod,
+      date,
+      time,
+      bookingStatus: "active",
+    });
+
+    await newBooking.save();
+
+    // Update occupied seats
+    if (!foundShow.occupiedSeats.has(date)) {
+      foundShow.occupiedSeats.set(date, new Map());
+    }
+    const dateMap = foundShow.occupiedSeats.get(date);
+    const currentSeats = dateMap.get(time) || [];
+    dateMap.set(time, [...new Set([...currentSeats, ...seats])]);
+    foundShow.occupiedSeats.set(date, dateMap);
+
+    await foundShow.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Booking created successfully!",
+      booking,
+    });
+  } catch (error) {
+    console.error("Error creating booking:", error);
+    res.status(500).json({success: false, message: "Server error"});
+  }
+};
+
+// get my bookings
+export const getMyBookings = async (req, res) => {
+  try {
+    const userId = req.auth().userId;
+    if (!userId) return res.status(401).json({success: false, message: "Unauthorized"});
+
+    // populate only 'show' because your Show schema does not have 'movie'
+    const bookings = await booking.find({user: userId}).populate({
+      path: "show",
+      model: "Show",
+      select: "title poster runtime showDateTime", // only these fields
+    });
+
+    res.json({success: true, bookings});
+  } catch (error) {
+    console.error("Error fetching user bookings:", error);
+    res.status(500).json({success: false, message: "Server error"});
+  }
+};
+
+// get all bookings
+export const getAllBookings = async (req, res) => {
+  try {
+    const bookings = await booking
+      .find()
+      .populate({
+        path: "user",
+        model: "User",
+        select: "name email",
+      })
+      .populate({
+        path: "show",
+        model: "Show",
+        select: "title ",
+      })
+      .sort({createdAt: -1});
+
+    res.status(200).json({success: true, bookings});
+  } catch (error) {
+    console.error("Error fetching all bookings:", error);
+    res.status(500).json({success: false, message: "Server error"});
+  }
+};
